c1a983745f41b8d367a2a6509d486be0615e9939
Development Van Peer <dieter@vanpeerdevelopment.be>
2014-04-21 16:30:56 +0200

    Add search event handlers for button click and search field enter

---
+++ blog/js/search.js
@@ -0,0 +1,22 @@
+// Search in title, tags, content
+function search() {
+    alert('Searching...');
+};
+
+// Initialize search function on page load.
+$(document).ready(function() {
+	// Register search event handler for search button
+	$("#search-button").click(function(event){
+		event.preventDefault();
+		search();
+	});
+
+	// Register search event handler for search field on enter.
+	$("#search-input").keypress(function(event) {
+		var enter_key_code = 13;
+		if(event.which === enter_key_code){
+			event.preventDefault();
+			search();
+		}
+	});
+});
